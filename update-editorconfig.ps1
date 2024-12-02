c49723a4d36b7062b319208512e374bd81393b19
SCISA\ibrahim.esparza <ibrahim.esparza@scisa.com.mx>
2024-12-02 17:30:01 -0600

    halo

--- update-editorconfig.ps1
+++ update-editorconfig.ps1
@@ -1,14 +1,28 @@
-# Obtener la ubicación del archivo Directory.Build.targets
-$directoryBuildTargetsPath = "C:\SCISA\SCISA_GIT\SCISA_BASE_APP" # Reemplaza con la ruta de tu carpeta
+# Buscar el archivo Directory.Build.targets en el directorio actual o en sus subdirectorios
+function Find-DirectoryBuildTargets {
+    $currentDir = Get-Location
+    Write-Host "Buscando Directory.Build.targets desde: $currentDir"
+    while (-not (Get-ChildItem -Path $currentDir -Filter "Directory.Build.targets" -File -ErrorAction SilentlyContinue)) {
+        $parentDir = $currentDir.Parent
+        if (-not $parentDir) {
+            throw "No se encontró el archivo Directory.Build.targets en la jerarquía de directorios desde $currentDir."
+        }
+        $currentDir = $parentDir
+    }
+    return $currentDir
+}
 
-# Validar si la carpeta existe
-if (-not (Test-Path $directoryBuildTargetsPath)) {
-    Write-Host "Error: No se encontró la carpeta donde está el Directory.Build.targets: $directoryBuildTargetsPath"
+# Obtener la ruta donde se encuentra Directory.Build.targets
+try {
+    $solutionRoot = Find-DirectoryBuildTargets
+    Write-Host "Directory.Build.targets encontrado en: $solutionRoot"
+} catch {
+    Write-Host "Error: $($_.Exception.Message)"
     exit 1
 }
 
 # Ruta donde se guardará el archivo .editorconfig
-$editorConfigPath = Join-Path $directoryBuildTargetsPath ".editorconfig"
+$editorConfigPath = Join-Path $solutionRoot ".editorconfig"
 
 # Ruta del archivo centralizado
 $sharedEditorConfigUrl = "https://raw.githubusercontent.com/ReParade/Git_Prueba/refs/heads/main/.editorconfig"
@@ -17,4 +31,4 @@ $sharedEditorConfigUrl = "https://raw.githubusercontent.com/ReParade/Git_Prueba/
 Write-Host "Descargando .editorconfig en: $editorConfigPath..."
 Invoke-WebRequest -Uri $editorConfigUrl -OutFile $editorConfigPath -UseBasicParsing
 
-Write-Host "Archivo .editorconfig actualizado correctamente en $editorConfigPath."
+Write-Host "Archivo .editorconfig actualizado correctamente."
