be7c04370e98892823a3300c3a7a953117c9d2e2
SCISA\ibrahim.esparza <ibrahim.esparza@scisa.com.mx>
2024-12-02 17:09:21 -0600

    guardar editorcofig en la raiz del proyecto

--- update-editorconfig.ps1
+++ update-editorconfig.ps1
@@ -1,7 +1,7 @@
 # Ruta del archivo centralizado
 $sharedEditorConfigUrl = "https://raw.githubusercontent.com/ReParade/Git_Prueba/refs/heads/main/.editorconfig"
 # Ruta local del archivo
-$localEditorConfig = ".editorconfig"
+$editorConfigPath = Join-Path $solutionRoot ".editorconfig"
 
 # Descargar si no existe o actualizar
 if (-Not (Test-Path $localEditorConfig) -or (Invoke-WebRequest -Uri $sharedEditorConfigUrl -Method Head).Headers["Last-Modified"] -ne (Get-Item $localEditorConfig).LastWriteTime) {
