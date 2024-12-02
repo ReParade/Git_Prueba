546cb3909e2c897a45c8c841819b9305904bf947
SCISA\ibrahim.esparza <ibrahim.esparza@scisa.com.mx>
2024-12-02 16:53:47 -0600

    Add update-editorconfig.ps1

---
+++ update-editorconfig.ps1
@@ -0,0 +1,10 @@
+# Ruta del archivo centralizado
+$sharedEditorConfigUrl = "https://raw.githubusercontent.com/ReParade/Git_Prueba/refs/heads/main/.editorconfig"
+# Ruta local del archivo
+$localEditorConfig = ".editorconfig"
+
+# Descargar si no existe o actualizar
+if (-Not (Test-Path $localEditorConfig) -or (Invoke-WebRequest -Uri $sharedEditorConfigUrl -Method Head).Headers["Last-Modified"] -ne (Get-Item $localEditorConfig).LastWriteTime) {
+    Invoke-WebRequest -Uri $sharedEditorConfigUrl -OutFile $localEditorConfig
+    Write-Host "Archivo .editorconfig actualizado."
+}
