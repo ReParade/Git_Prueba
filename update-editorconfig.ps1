b0fa8c62a0cd2172badc1a01624b7c5e6405156e
SCISA\ibrahim.esparza <ibrahim.esparza@scisa.com.mx>
2024-12-02 17:25:40 -0600

    se guardara solo en el Directory.Build.targets

--- update-editorconfig.ps1
+++ update-editorconfig.ps1
@@ -1,33 +1,20 @@
-# Buscar la raíz de la solución (.sln más cercano)
-function Get-SolutionRoot {
-    $currentDir = Get-Location
-    Write-Host "Buscando archivo .sln desde: $currentDir"
-    while (-not (Get-ChildItem -Path $currentDir -Filter "*.sln" -File -ErrorAction SilentlyContinue)) {
-        $parentDir = $currentDir.Parent
-        if (-not $parentDir) {
-            throw "No se encontró un archivo .sln en la jerarquía de directorios desde $currentDir."
-        }
-        $currentDir = $parentDir
-    }
-    return $currentDir
-}
+# Obtener la ubicación del archivo Directory.Build.targets
+$directoryBuildTargetsPath = "C:\SCISA\SCISA_GIT\SCISA_BASE_APP" # Reemplaza con la ruta de tu carpeta
 
-# Obtener la ruta de la raíz de la solución
-try {
-    $solutionRoot = Get-SolutionRoot
-    Write-Host "Raíz de la solución encontrada en: $solutionRoot"
-} catch {
-    Write-Host "Error: $($_.Exception.Message)"
+# Validar si la carpeta existe
+if (-not (Test-Path $directoryBuildTargetsPath)) {
+    Write-Host "Error: No se encontró la carpeta donde está el Directory.Build.targets: $directoryBuildTargetsPath"
     exit 1
 }
 
+# Ruta donde se guardará el archivo .editorconfig
+$editorConfigPath = Join-Path $directoryBuildTargetsPath ".editorconfig"
+
 # Ruta del archivo centralizado
 $sharedEditorConfigUrl = "https://raw.githubusercontent.com/ReParade/Git_Prueba/refs/heads/main/.editorconfig"
-# Ruta local del archivo
-$editorConfigPath = Join-Path $solutionRoot ".editorconfig"
 
 # Descargar el archivo .editorconfig
-Write-Host "Descargando .editorconfig en $editorConfigPath..."
+Write-Host "Descargando .editorconfig en: $editorConfigPath..."
 Invoke-WebRequest -Uri $editorConfigUrl -OutFile $editorConfigPath -UseBasicParsing
 
-Write-Host "Archivo .editorconfig actualizado correctamente."
+Write-Host "Archivo .editorconfig actualizado correctamente en $editorConfigPath."
